26d8913b4453a0437983ae9db05ecaefedfc350b
Kia Fathi <kiafathi@me.com>
2014-07-07 13:30:39 -0700

    Change file structure for better modularity

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("myapp", ["firebase"]);
+
+function MyController($scope, $firebase) {
+  var ref = new Firebase("https://OpFed.firebaseio.com/");
+  $scope.messages = $firebase(ref);
+  $scope.addMessage = function(e) {
+    if (e.keyCode != 13) return;
+    $scope.messages.$add({from: $scope.name, body: $scope.msg});
+    $scope.msg = "";
+  };
+}
