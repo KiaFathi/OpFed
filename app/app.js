921dcf8d98ce3ec4f078759c8c1f05ccc1cfdcdb
Kia Fathi <kiafathi@me.com>
2014-07-07 16:57:52 -0700

    Add image files

--- app/app.js
+++ app/app.js
@@ -17,7 +17,6 @@ var app = angular.module("myapp", ["firebase"])
           hate: 0,
           votes: 0,
           comments: {},
-          showComments: false
         })
         $scope.title ='';
       };
@@ -28,50 +27,16 @@ var app = angular.module("myapp", ["firebase"])
        var comment = prompt("Do you have a comment?");
        if(this.topic.comments){
          this.topic.comments[this.topic.votes] = sentiment + ": "+comment;
-       } else{
+       } else {
         var obj = {};
         obj[this.topic.votes] = sentiment + ": " + comment;
         this.topic.comments = obj;
        }
       };
 
-      $scope.showComments = function(){
-        console.log(this.topic.showComments);
-
-        if(this.topic.showComments){
-          this.topic.showComments = false;
-        }else{
-          this.topic.showComments = true;
-        }
-      }
 
       $scope.sayHi = function(){
         alert('Hi!');
       };
     }
   ]);
-
-
-// function TopicController($scope, $firebase) {
-//   var ref = new Firebase("https://OpFed.firebaseio.com/");
-//   $scope.topics = $firebase(ref);
-//   $scope.addTopic = function(e) {
-//     if (e.keyCode != 13) return;
-//     $scope.topics.$add({
-//       title: $scope.title,
-//       love: 0,
-//       like: 0,
-//       dislike: 0,
-//       hate: 0
-//     });
-//     $scope.topic = "";
-//   };
-//   $scope.vote = function(sentiment){
-//     console.log(sentiment.$name());
-//     this.love++;
-//   }
-
-//   $scope.sayHi = function(){
-//     alert('Hi!');
-//   }
-// }
