e2555afe148bed134da5bb2b2e369a46961fda6a
Kia Fathi <kiafathi@me.com>
2014-07-08 11:16:29 -0700

    Set up routing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
 angular.module('OpFed', [
-  'OpFed.controllers', "firebase"])
+  'OpFed.controllers',
+   'firebase',
+   'ngRoute'])
   .factory("database", ["$firebase", function($firebase) {
     var ref = new Firebase("https://OpFed.firebaseio.com/topics");
     return $firebase(ref);
+  }])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+      .when("/login", {
+        templateUrl: "./partials/login.html", 
+        controller: "loginController"
+      })
+      .when("/topics", {
+        templateUrl: "./partials/topics.html",
+        controller: "topicsController"
+      })
+      .otherwise({
+        redirectTo: '/topics'
+      });
   }]);
