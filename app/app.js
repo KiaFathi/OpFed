f125d850c4684ddbca3e88d9056f8228cba1033c
Kia Fathi <kiafathi@me.com>
2014-07-07 19:53:55 -0700

    Update basic functionality to have comments and voting system work properly

--- app/app.js
+++ app/app.js
@@ -1,33 +1,12 @@
-var username;
-var dbref = new Firebase('https://opfed.firebaseio.com');
-var auth = new FirebaseSimpleLogin(dbref, function(error, user) {
-  if (error) {
-    // an error occurred while attempting login
-    console.log(error);
-  } else if (user) {
-    // user authenticated with Firebase
-    username = user.displayName;
-    console.log(username);
-  } else {
-    // user is logged out
-    auth.login('github');
-    console.log(user);
-  }
-});
-
-var app = angular.module("myapp", ["firebase"])
+  var app = angular.module("myapp", ["firebase"])
   .factory("database", ["$firebase", function($firebase) {
     var ref = new Firebase("https://OpFed.firebaseio.com/topics");
-    var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-    });
     return $firebase(ref);
   }])
   .controller("TopicController", ["$scope", "database",
     function($scope, service) {
       service.$bind($scope, "topics");
 
-      $scope.username = username;
-      
       $scope.addTopic = function(e) {
         if (e.keyCode != 13) return;
         service.$add({
@@ -43,21 +22,17 @@ var app = angular.module("myapp", ["firebase"])
       };
 
       $scope.vote = function(sentiment){
-        if(!this.topic.comments[username]){
-          this.topic[sentiment]++;
-          this.topic.votes++;
-          var comment = prompt("Write a comment");
-          if(comment){
-            if(this.topic.comments){
-              this.topic.comments[username] = username + ": "+comment;
-            } else {
-              var obj = {};
-              obj[username] = username + ": " + comment;
-              this.topic.comments = obj;
-            }
+        this.topic[sentiment]++;
+        this.topic.votes++;
+        var comment = prompt("Write a comment");
+        if(comment){
+          if(this.topic.comments){
+            this.topic.comments[comment] = sentiment + ": "+comment;
+          } else {
+            var obj = {};
+            obj[comment] = sentiment + ": " + comment;
+            this.topic.comments = obj;
           }
-        } else{
-          alert('You already voted on this');
         }
       };
     }
